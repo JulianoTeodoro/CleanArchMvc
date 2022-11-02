f74b5c9e63045c08ea206086091eeb5a82a6e760
Juliano Teodoro <julianoteodoro2002@gmail.com>
2022-11-01 21:32:07 -0300

    Api concluida

---
+++ CleanArchMvc.WebAPI/Controllers/CategorysController.cs
@@ -0,0 +1,102 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategorysController: Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategorysController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet(Name = "Obter Categoria")]
+        public async Task<ActionResult<List<CategoryDTO>>> Get()
+        {
+            try
+            {
+                var categorys = await _categoryService.GetCategorys();
+                return Ok(categorys);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Erro ao consultar" });
+            }
+        }
+
+        [HttpGet("{id:int:min(1):maxlength(5)}")]
+        public async Task<ActionResult<List<CategoryDTO>>> GetById(int id)
+        {
+            try
+            {
+                var category = await _categoryService.GetById(id);
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Erro ao consultar" });
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CategoryDTO>> Post(CategoryDTO category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Erro ao enviar");
+            }
+            try
+            {
+                await _categoryService.Add(category);
+                return new CreatedAtRouteResult("Obter Categoria", new { category = category.Id }, category);
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest("Erro ao enviar");
+            }
+        }
+
+        [HttpPut("{id:int:min(1):maxlength(5)}")]
+        public async Task<ActionResult<CategoryDTO>> Put(int? id, CategoryDTO category)
+        {
+            if (id is null || !ModelState.IsValid)
+            {
+                return BadRequest("Erro ao editar");
+            }
+            try
+            {
+                await _categoryService.Update(category);
+                return Ok(category);
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Erro ao editar" });
+            }
+        }
+
+        [HttpDelete("{id:int:min(1):maxlength(5)}")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest("Erro ao remover");
+            }
+            try
+            {
+                await _categoryService.Remove((int)id);
+                return Ok("Categoria removida");
+            }
+            catch (Exception)
+            {
+                return BadRequest("Erro ao remover");
+            }
+        }
+    }
+}
---
+++ CleanArchMvc.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,117 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+
+namespace CleanArchMvc.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet(Name = "Obter Produto")]
+        public async Task<ActionResult<List<ProductDTO>>> Get()
+        {
+            try
+            {
+                var products = await _productService.GetProducts();
+                return Ok(products);
+            }
+            catch(Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Erro ao consultar" });
+            }
+        }
+
+        [HttpGet("productCategory/{id:int:min(1):maxlength(5)}")]
+        public async Task<ActionResult<List<ProductDTO>>> GetProductCategory(int id)
+        {
+            try
+            {
+                var product = await _productService.GetProductCategory(id);
+                return Ok(product);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Erro ao consultar" });
+            }
+        }
+
+        [HttpGet("{id:int:min(1):maxlength(5)}")]
+        public async Task<ActionResult<List<ProductDTO>>> GetById(int id)
+        {
+            try
+            {
+                var product = await _productService.GetById(id);
+                return Ok(product);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Erro ao consultar" });
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDTO>> Post(ProductDTO product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Erro ao enviar");
+            }
+            try
+            {
+                await _productService.Add(product);
+                return new CreatedAtRouteResult("Obter Produto", new { product = product.Id }, product);
+            }
+            catch(DbUpdateException)
+            {
+                return BadRequest("Erro ao enviar");
+            }
+        }
+
+        [HttpPut("{id:int:min(1):maxlength(5)}")]
+        public async Task<ActionResult<ProductDTO>> Put(int? id, ProductDTO product)
+        {
+            if(id is null || !ModelState.IsValid)
+            {
+                return BadRequest("Erro ao editar");
+            }
+            try
+            {
+                await _productService.Update(product);
+                return Ok(product);
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Erro ao editar" });
+            }
+        }
+
+        [HttpDelete("{id:int:min(1):maxlength(5)}")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest("Erro ao remover");
+            }
+            try
+            {
+                await _productService.Remove((int)id);
+                return Ok("Produto removido");
+            }
+            catch(Exception)
+            {
+                return BadRequest("Erro ao remover");
+            }
+        }
+    }
+}
